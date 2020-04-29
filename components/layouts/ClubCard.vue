f2a6ca73c9ed855cf6dad8eae5bf623377689d8e
SoraY677 <SoraY677@outlook.com>
2020-04-29 13:16:50 +0900

    [update]サークル紹介/要件確定

--- components/layouts/ClubCard.vue
+++ components/layouts/ClubCard.vue
@@ -1,29 +1,51 @@
 <template>
-  <div class="container fill-height">
+  <v-card class="fill-height">
+    <!-- タイトル -->
+    <v-card-text>
+      <p>●●団体</p>
+      <p class="display-1 text--primary">
+        {{ content.name }}
+      </p>
+    </v-card-text>
+    <!--リンク集-->
+    <v-card-actions>
+      <v-btn v-if="!Object.keys(content.link).length" disabled text></v-btn>
+      <v-btn
+        v-for="(value, key) in content.link"
+        :key="key"
+        @click="transLink(value)"
+        text
+      >
+        <v-icon>{{ iconPathList[key] }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+    <!-- 説明文 -->
+    <v-card-text class="text--primary">
+      {{ content.overview }}
+    </v-card-text>
+  </v-card>
+  <!-- <div class="container fill-height">
     <v-card class="layout" outlined>
       <v-container fluid>
-      <v-img
-        height="250"
-        class="white--text align-end"
-        :src="content.imgpath"
-        float="left"
-      ></v-img>
 
-      <v-card-title class="pb-0">{{ content.name }}</v-card-title>
+        <v-card-title class="pb-0">{{ content.name }}</v-card-title>
 
-
-      <v-card-actions >
-        <v-btn v-for="(value,key) in content.link" :key="key" @click="transLink(value)" text>
-          <v-icon>{{iconPathList[key]}}</v-icon>
-        </v-btn>
-      </v-card-actions>
-      <v-card-text class="text--primary">
+        <v-card-actions>
+          <v-btn
+            v-for="(value, key) in content.link"
+            :key="key"
+            @click="transLink(value)"
+            text
+          >
+            <v-icon>{{ iconPathList[key] }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+        <v-card-text class="text--primary">
           {{ content.overview }}
-        
-      </v-card-text>
+        </v-card-text>
       </v-container>
     </v-card>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -34,18 +56,17 @@ export default {
   data() {
     return {
       iconPathList: {
-        hp:"fas fa-home",
-        twitter:"fab fa-twitter"
+        hp: "fas fa-home",
+        twitter: "fab fa-twitter"
       }
     };
   },
-  methods:{
-    transLink(path){
-      window.open(path,"_blank")
+  methods: {
+    transLink(path) {
+      window.open(path, "_blank");
     }
   }
 };
 </script>
 
-<style>
-</style>
+<style></style>
