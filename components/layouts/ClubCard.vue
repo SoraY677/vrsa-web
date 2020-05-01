e5063f699d76ed5318a82e74937704e18b2d8461
SoraY677 <SoraY677@outlook.com>
2020-05-01 17:26:56 +0900

    [update]ModalWindowの調整

--- components/layouts/ClubCard.vue
+++ components/layouts/ClubCard.vue
@@ -7,18 +7,9 @@
     <v-card-subtitle>
       {{ content.genre }}
     </v-card-subtitle>
-    <v-img height="200px" :src="content.imgpath"> </v-img>
-    <!--リンク集-->
-    <!-- <v-card-actions>
-      <v-btn
-        v-for="(value, key) in content.link"
-        :key="key"
-        @click="transLink(value)"
-        text
-      >
-        <v-icon>{{ iconPathList[key] }}</v-icon>
-      </v-btn>
-    </v-card-actions> -->
+    <v-container>
+      <v-img height="200px" :src="content.imgpath"> </v-img>
+    </v-container>
     <ClubModal ref="club_modal" :content="content" />
   </v-card>
 </template>
@@ -30,21 +21,10 @@ export default {
   props: {
     content: Object
   },
-  data() {
-    return {
-      iconPathList: {
-        hp: "fas fa-home",
-        twitter: "fab fa-twitter"
-      }
-    };
-  },
   components: {
     ClubModal
   },
   methods: {
-    transLink(path) {
-      window.open(path, "_blank");
-    },
     openModal() {
       this.$refs.club_modal.open();
     }
--- components/layouts/ClubModal.vue
+++ components/layouts/ClubModal.vue
@@ -1,23 +1,85 @@
 <template>
-  <div>
-    <v-dialog v-model="isShow" persistent>
-      いｗ－やｗ－さｗーさｗー
-      <v-btn @click="isShow = false" text></v-btn>
-    </v-dialog>
-  </div>
+  <v-dialog color="white" v-model="isShow">
+    <v-card>
+      <v-container style="position:relative; max-width:90%">
+        <v-btn
+          color="blue"
+          absolute
+          dark
+          right
+          fab
+          @click="isShow = false"
+          text
+          x-large
+        >
+          <v-icon>mdi-close</v-icon>
+          <div>close</div>
+        </v-btn>
+        <v-card-title class="display-1 text--primary">
+          {{ content.name }}
+        </v-card-title>
+        <v-card-subtitle>{{ content.genre }}</v-card-subtitle>
+        <!--リンク集-->
+        <v-card-actions>
+          <v-btn
+            v-for="(value, key) in content.link"
+            :key="key"
+            @click="transLink(value)"
+            text
+          >
+            <v-icon>{{ iconPathList[key] }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+        <v-divider></v-divider>
+        <v-container>
+          <!-- パンフ画像 -->
+          <v-img
+            class="mx-auto"
+            :max-width="imgMaxSize"
+            width="100%"
+            height="100%"
+            :src="content.imgpath"
+          >
+          </v-img>
+          <p>{{ content.overview }}</p>
+        </v-container>
+      </v-container>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
+  props: {
+    content: {}
+  },
   data() {
     return {
-      isShow: false
+      isShow: false,
+      iconPathList: {
+        hp: "fas fa-home",
+        twitter: "fab fa-twitter"
+      },
+      imgMaxSize: ""
     };
   },
+  created() {
+    let img = new Image();
+    img.src = this.content.imgpath;
+    console.log(img.width);
+    console.log(img.height);
+    if (img.width < img.height) {
+      this.imgMaxSize = "400px";
+    } else {
+      this.imgMaxSize = "";
+    }
+  },
   methods: {
+    transLink(path) {
+      window.open(path, "_blank");
+    },
     open() {
       this.isShow = true;
-      console.log("open b");
     }
   }
 };
